e6a55d6ae3d6d83d6f185f5f4b28271499862fdf
Fermedina <fermed28@gmai.com>
2017-12-11 21:07:45 -0500

    add devise functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'questions#index'
+  devise_for :users
+  resource :questions
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
