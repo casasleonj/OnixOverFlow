f430a7a2973be5973f3e95346f6310a82d3d4fc8
Fermedina <fermed28@gmai.com>
2017-12-11 23:01:14 -0500

    set models answer and question with commentable

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,4 @@
 class Answer < ApplicationRecord
   belongs_to :user
+  has_many :comment, :as => :commentable
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :user
+  has_many :comment, :as => :commentable
 end
